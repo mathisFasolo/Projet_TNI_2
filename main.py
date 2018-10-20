4dd735c17b940a799760c6f023fb927ab7498e49
mathisFasolo <mathis.fasolo@icloud.com>
2018-10-20 17:04:01 +0200

    nouvelle fonction de creation de dictionnaire correction de la fonction d'encodage calcul du taux de compression

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import itertools
 from models.Text import Text
 from models.Pixel import Pixel
 
@@ -17,11 +18,30 @@ def codage_arithmetique(dict_pixel_proba, list_length):
 
 
 if __name__ == '__main__':
-    print(Text.generer_dico_digramme())
+    # transform text to ascii
+    Text.transformationText()
+    # genere 3 dictionnaire en fonction des unigramme, digramme et trigramme
+    dicoUnigram = Text.generer_dico_ngramme(1)
+    dicoDigram = Text.generer_dico_ngramme(2)
+    dicoTrigram = Text.generer_dico_ngramme(3)
+    print(len(dicoUnigram))
+    print(len(dicoDigram))
+    print(len(dicoTrigram))
+    # associe un byte à un n_gramme dans un nouveau dictionnaire
+    dicoNgram = Text.creerDictionnaire(dicoUnigram, dicoDigram[:100], dicoTrigram[:98])
+    print(len(dicoNgram))
+    finalDictionnary = Text.generateByteList(dicoNgram)
+    print(finalDictionnary)
 
+    newText = Text.encode(finalDictionnary)
+    with open("./data/text_ascii_compressed", 'wb+') as compressed_file:
+        for bytes in newText:
+            compressed_file.write(bytes)
+    print(Text.tauxDeCompression())
+
+    #print(Text.encode(dicoUnigram, dicoDigram))
     # dictPixel = Pixel.getDictPixel()
     # packets = list(Pixel.splitListValuesByPacket(dictPixel))
     # print(len(packets))
     # for packet in packets:
     #     print(packet)
-    Text.transformationText()
--- models/Text.py
+++ models/Text.py
@@ -1,8 +1,11 @@
 from collections import Counter
 import math
+import json
+from operator import itemgetter, attrgetter
 import unicodedata
+import unidecode
 
-PATH_FILE_TXT = "./data/exemple1.txt"
+PATH_FILE_TXT = "./data/text_ascii.txt"
 
 
 class Text():
@@ -49,31 +52,95 @@ class Text():
     def transformationText():
         with open(PATH_FILE_TXT, 'r') as text:
             brut = text.read()
-            traite = unicodedata.normalize('NFD', brut).encode('ascii', 'ignore')
+            #traite = unicodedata.normalize('NFD', brut).encode('ascii', 'ignore')
+            traite = unidecode.unidecode(brut)
             with open("./data/text_ascii.txt", "w") as outfile:
                 outfile.write(str(traite))
 
-
+    '''
+        Cette fonction trie genere un tableau de dictionnaire avec une association 
+        n_gram / occurence dans le texte de tallgrand
+        @:param Taille n_gram
+        @:returns list
+    '''
     @staticmethod
-    def generer_dico_digramme():
+    def generer_dico_ngramme(taille_n):
         with open(PATH_FILE_TXT, "r") as text:
             texte = text.read()
             i = 0
             seen = []
-
             dico = {}
-            while i < len(texte):
-                if(i)
-                tempDiagramme = texte[i]+texte[i+1]
-                print(tempDiagramme)
+            result = []
+            bound = taille_n - 1
+            while i < len(texte) - bound:
+                tempDiagramme = ""
+                for j in range(taille_n):
+                    tempDiagramme = tempDiagramme + texte[i+j]
                 if tempDiagramme in seen:
                     dico[tempDiagramme] = dico[tempDiagramme] + 1
                 else:
                     dico[tempDiagramme] = 1
                     seen.append(tempDiagramme)
                 i = i + 1
-            return dico
+            for key, value in dico.items():
+                result.append({key: value})
+            return sorted(result, reverse=True, key=lambda dictionnarie: list(dictionnarie.values())[0])
 
+    @staticmethod
+    def generateByteList(list_dict_ngram):
+        dictResult = {}
+        i = 0
+        if len(list_dict_ngram) > 256:
+            list_dict_ngram = list_dict_ngram[:255]
+        for dict_ngram in list_dict_ngram:
+            dictResult[list(dict_ngram.keys())[0]] = bytes([i])
+            #dictResult[list(dict_ngram.keys())[0]] = i
+            i = i + 1
+        return dictResult
 
+    @staticmethod
+    def saveDictionnaire(dictionnaire):
+        with open('./dictionnaire/Dictionnaire.json', 'w+') as final_file:
+            json.dump(dictionnaire, final_file)
 
+    @staticmethod
+    def creerDictionnaire(listUnigram = list(), listDigram = list(), listTrigram = list()):
+        result = []
+        for unigram in listUnigram:
+            result.append({list(unigram.keys())[0]: list(unigram.values())[0]})
+        for digram in listDigram:
+            result.append({list(digram.keys())[0]: list(digram.values())[0]})
+        for trigram in listTrigram:
+            result.append({list(trigram.keys())[0]: list(trigram.values())[0]})
+        return result
 
+    @staticmethod
+    def encode(dictionnaire):
+        with open(PATH_FILE_TXT, 'r') as file:
+            result = []
+            i = 0
+            text = file.read()
+            while i < len(text) - 3:
+                bound = 0
+                for j in reversed(range(4)):
+                    temp_naire = text[i:i + j]
+                    if temp_naire in dictionnaire:
+                        result.append(dictionnaire[temp_naire])
+                        bound = j
+                        break
+                i = i + bound
+            return result
+
+    @staticmethod
+    def tauxDeCompression():
+        nbBytesUncompressed = 1
+        nbBytesCompressed = 0
+        with open(PATH_FILE_TXT, 'rb') as uncompressedFile:
+            data = uncompressedFile.read()
+            print("Taille fichier origine: %d" % len(data))
+            nbBytesUncompressed = len(data)
+        with open("./data/text_ascii_compressed", 'rb') as compressedFile:
+            data = compressedFile.read()
+            print("Taille fichier compressé: %d" % len(data))
+            nbBytesCompressed = len(data)
+        return (nbBytesCompressed/nbBytesUncompressed) * 100
